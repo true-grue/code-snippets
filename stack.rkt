8cb6aebdfb3e747ac9a3257e110f79069ce655c6
Peter Sovietov <peter.sovietov@gmail.com>
2019-12-13 20:07:52 +0300

    simpler code

--- stack.rkt
+++ stack.rkt
@@ -6,12 +6,11 @@
   (op ::= + - * / dup drop swap)
   (lit ::= number)
   (cmd ::= op lit)
-  (prog ::= (cmd ...))
-  (st ::= (lit ...)))
+  (prog ::= [(cmd ...) (lit ...)]))
 
 (define stack-red
   (reduction-relation stack-lang
-   #:domain [prog st]
+   #:domain prog
    (--> [(lit_1 cmd ...) (lit ...)]
         [(cmd ...) (lit_1 lit ...)]
         "literal")
@@ -39,8 +38,7 @@
    (--> [(/ cmd ...) (lit_1 lit_2 lit ...)]
         [(cmd ...) (lit_3 lit ...)]
         (where lit_3 ,(/ (term lit_2) (term lit_1)))
-        "/")
-   ))
+        "/")))
 
 (test-->> stack-red (term [(dup * 1 -) (4)]) (term [() (15)]))
   
