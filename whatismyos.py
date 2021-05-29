3746f0f499c06eedf8e2a52a88c067da92a213e9
Peter Sovietov <peter.sovietov@gmail.com>
2021-05-29 23:28:31 +0300

    what is my os

---
+++ whatismyos.py
@@ -0,0 +1,10 @@
+﻿def whatismyos():
+    from struct import unpack
+    try:
+        unpack('L', bytes([0] * 4))
+        return 'Windows'
+    except:
+        return 'Linux'
+
+
+print(whatismyos())
